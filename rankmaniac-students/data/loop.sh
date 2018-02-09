1f40a96f97a02785e43985511c77e32e97876ce2
amine000 <boubezari.amine@gmail.com>
2018-02-08 23:08:22 -0800

    safe commit

--- rankmaniac-students/data/loop.sh
+++ rankmaniac-students/data/loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {1..5}
+for i in {1..3}
 do
 echo “iteration $i done”;
-   python pagerank_map.py < input.txt | sort | python pagerank_reduce.py | python process_map.py | python process_reduce.py > out.txt
+   python pagerank_map.py < outgnp.txt | sort | python pagerank_reduce.py | python process_map.py | python process_reduce.py > outgnp2.txt
 done
